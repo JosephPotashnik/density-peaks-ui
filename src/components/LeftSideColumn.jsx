37c701880c113a6aca094bcd421a675d06e85031
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-02-27 14:50:30 +0000

    clustering icon, preliminary areas defined

--- src/components/LeftSideColumn.jsx
+++ src/components/LeftSideColumn.jsx
@@ -5,7 +5,7 @@ export function LeftSideColumn() {
     return (
       <>
         <div className="leftSideColumn"> 
-        <p>left side</p>
+        <h2>Datasets</h2>
       <Button text="Dataset 1" filename="dataset1.txt" />
       <Button text="Dataset 2" filename="dataset2.txt" />
       <Button text="Dataset 3" filename="dataset3.txt" />
